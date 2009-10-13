145b2aca58ede0a07319a964f95e2da573057f31
Will Donnelly <will.donnelly@gmail.com>
2009-10-12 19:07:31 -0500

    Cleanups

--- BriansBrain.hs
+++ BriansBrain.hs
@@ -1,36 +1,35 @@
 import Data.List
 
-data CellState = CellOff | CellDying | CellOn deriving (Eq, Ord, Enum, Show)
-data Neighbors = Neighbors CellState [CellState] deriving (Eq, Show)
-data World     = World Int Int [[CellState]]
+data Cell  = CellOff | CellDying | CellOn deriving (Eq, Ord, Enum, Show)
+data Peers = Peers Cell [Cell] deriving (Eq, Show)
+data World = World Int Int [[Cell]]
+
+stepWorld :: World -> World
+stepWorld world@(World mx my _) = World mx my $ split (mx + 1) cells
+  where cells   = [ newCell (x,y) | x <- [0..mx], y <- [0..my] ]
+        newCell = stepCell . countPeers . getPeers world
+        split n = unfoldr splitNext
+          where splitNext [] = Nothing
+                splitNext xs = Just $ splitAt n xs
+
+stepCoord :: World -> (Int, Int) -> Cell
+stepCoord world = stepCell . countPeers . getPeers world
+  where countPeers (Peers c cs) = (c, length . filter (== CellOn) $ cs)
 
 wrapCoord :: World -> (Int, Int) -> (Int, Int)
 wrapCoord (World mx my cells) (x, y) = (wrapNum mx x, wrapNum my y)
-  where wrapNum max val
-            | val > max  = val - max
-            | val < 0    = val + max
-            | otherwise  = val
+  where wrapNum max val | val > max  = val - max
+                        | val < 0    = val + max
+                        | otherwise  = val
 
-getNeighbors :: World -> (Int, Int) -> Neighbors
-getNeighbors world (x, y) = Neighbors center [nw,n,ne,w,e,sw,s,se]
+getPeers :: World -> (Int, Int) -> Peers
+getPeers world (x, y) = Peers center [nw,n,ne,w,e,sw,s,se]
   where [nw,n,ne,w,center,e,sw,s,se] = map cellVal offsets
         offsets = [ (x + a, y + b) | a <- [-1..1], b <- [-1..1] ]
         cellVal = getCell world . wrapCoord world
         getCell (World mx my cells) (x, y) = cells !! x !! y
 
-stepWorld :: World -> World
-stepWorld world@(World mx my _) = World mx my $ split (mx + 1) cells
-  where cells   = [ newCell (x,y) | x <- [0..mx], y <- [0..my] ]
-        newCell = stepCell . countNeighbors . getNeighbors world
-        split n = unfoldr splitNext
-          where splitNext [] = Nothing
-                splitNext xs = Just $ splitAt n xs
-
-countNeighbors :: Neighbors -> (CellState, Int)
-countNeighbors (Neighbors c cs) = (c, numLiving cs)
-  where numLiving = length . filter (== CellOn)
-
-stepCell :: (CellState, Int) -> CellState
+stepCell :: (Cell, Int) -> Cell
 stepCell (CellOff,   2) = CellOn
 stepCell (CellOff,   _) = CellOff
 stepCell (CellDying, _) = CellOff

22834589023438c7b0b175b4cd159ce6e613524c
Will Donnelly <will.donnelly@gmail.com>
2009-10-13 23:09:59 -0500

    Rewrote to use arrays. It's fast now.

--- BriansBrain.hs
+++ BriansBrain.hs
@@ -1,13 +1,15 @@
 import Random ( newStdGen, randomRIO, randomRs, RandomGen )
 import Data.List
+import Data.Array
 import Control.Arrow
 import Graphics.UI.SDL hiding (index)
 import Control.Monad
 import Control.Concurrent
+import Control.Parallel.Strategies
 import Prelude hiding (flip)
 
 data Cell  = CellOff | CellDying | CellOn deriving (Eq, Ord, Enum, Show)
-data World = World Int Int [[Cell]]
+type World = Array (Int, Int) Cell
 type Peers = (Cell, Int)
 
 cellSize = 8
@@ -19,49 +21,48 @@ stepCell (CellOff,   _) = CellOff
 stepCell (CellDying, _) = CellOff
 stepCell (CellOn,    _) = CellDying
 
-index cells (x, y) = cells !! (y - 1) !! (x - 1)
+stepWorld :: World -> World
+stepWorld world = stepCell `fmap` peersArray world
+
+peersArray :: World -> Array (Int, Int) Peers
+peersArray world = getPeers world `fmap` indexArray x y
+  where ((1,1),(x,y)) = bounds world
+
+indexArray :: Int -> Int -> Array (Int, Int) (Int, Int)
+indexArray x y = array ((1,1),(x,y)) [((x,y),(x,y)) | x <- [1..x], y <- [1..y]]
 
 getPeers :: World -> (Int, Int) -> Peers
-getPeers (World mx my cells) (x, y) = (index cells (x,y), living indices)
-  where living   = length . filter (== CellOn) . map (index cells)
-        indices  = [wrap (x+a) (y+b) | b <- [-1..1], a <- [-1..1]]
-        wrap x y = (wrap' mx x, wrap' my y)
-          where wrap' max val | val > max  = val - max
-                              | val < 1    = val + max
-                              | otherwise  = val
+getPeers world pos@(x,y) = (world ! pos, living neighbors)
+  where neighbors = do
+            x <- [x-1 .. x+1]
+            y <- [y-1 .. y+1]
+            return $ world ! (clip (minX, maxX) x, clip (minY, maxY) y)
+        living = length . filter (== CellOn)
+        ((minX,minY),(maxX,maxY)) = bounds world
 
-stepWorld :: World -> World
-stepWorld world@(World mx my _) = World mx my $ split mx cells
-  where cells = [step (x,y) | y <- [1..my], x <- [1..mx]]
-        step  = stepCell . getPeers world
-
--- | A helper function that the standard Prelude seems
---   to lack.
-split :: Int -> [a] -> [[a]]
-split n = unfoldr (\x -> case x of [] -> Nothing; xs -> Just $ splitAt n xs)
+clip :: (Int, Int) -> Int -> Int
+clip bounds@(min, max) val
+  | val < min = clip bounds $ val + max - min
+  | val > max = clip bounds $ val + min - max
+  | otherwise = val
 
 randWorld :: RandomGen r => Int -> Int -> r -> World
-randWorld x y = World x y . take y . split x . map toEnum . randomRs (0,2)
+randWorld x y = array ((1,1),(x,y)) . zip indices . map toEnum . randomRs (0,2)
+  where indices = [(x,y) | x <- [1..x], y <- [1..y]]
 
 main = do
     world <- randWorld sizeX sizeY `fmap` newStdGen
     withInit [InitVideo] $ do
         setCaption title title
         surface <- setVideoMode (sizeX*cellSize) (sizeY*cellSize) 24 [DoubleBuf]
-        forM_ (take 10 $ iterate stepWorld world) $ \(World mx my cells) -> do
-            event <- pollEvent
-            case event of
-                Quit -> quit
-                _ -> return ()
+        forM_ (iterate stepWorld world) $ \world -> do
             sequence $ do
-                x <- [1..mx]
-                y <- [1..my]
-                return $ drawCell surface x y $ cells `index` (x,y)
+                x <- [1..sizeX]
+                y <- [1..sizeY]
+                return $ drawCell surface x y $ world ! (x,y)
             flip surface
---            threadDelay 250000
   where title = "Brian's Purely Functional Brain"
-        sizeX = 90
-        sizeY = 90
+        (sizeX, sizeY) = (90, 90)
 
 drawCell s x y cell = fillRect s (Just rect) $ color cell
   where color CellOn    = Pixel 0x00FFFFFF
